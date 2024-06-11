d86882855610278f7b8ccb46b24d196a308d1f1a
katherineleald <katleald0412@gmail.com>
2024-06-11 14:27:20 -0400

    commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "../app.css";
+
+  import { library, dom } from "@fortawesome/fontawesome-svg-core";
+  import { fas } from "@fortawesome/free-solid-svg-icons";
+
+  library.add(fas);
+  dom.watch();
+</script>
+
+<slot />
---
+++ src/stores/mealsStore.js
@@ -0,0 +1,4 @@
+// src/stores/mealsStore.js
+import { writable } from 'svelte/store';
+
+export const mealsStore = writable([]);
